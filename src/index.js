76c8ff6ae45052181519322ea3f2c6aa0403ed14
Maximilian Bethke <maxbethke@web.de>
2022-06-18 19:58:40 +0200

    generalize endpoint to query all available databases

--- src/index.js
+++ src/index.js
@@ -8,31 +8,39 @@ dotenv.config()
 
 const app = express();
 const port = 3001
+const notion = new Client({ auth: process.env.NOTION_KEY });
+
 app.use(cors());
-
-const notion = new Client({ auth: process.env.NOTION_KEY })
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/techstack', async (req, res) => {
-    const notionResponse = await queryDatabase(res, process.env.NOTION_DATABASE_ID_TECHSTACK)
+const DATABASE_IDS = {
+    techstack: process.env.NOTION_DATABASE_ID_TECHSTACK,
+    tools: process.env.NOTION_DATABASE_ID_TOOLS,
+    languages: process.env.NOTION_DATABASE_ID_LANGUAGES,
+    softskills: process.env.NOTION_DATABASE_ID_SOFTSKILLS
+}
 
-    const pages = notionResponse.results.map(page => {return {
-        name: page.properties.Name.title[0].plain_text,
-        exp: page.properties["Experience /10"].number,
-        field: page.properties.Field.select.name
-    }})
+app.get('/:database', async (req, res) => {
+    if(!DATABASE_IDS.hasOwnProperty(req.params.database)) res.status(404)
 
-    res.send(pages)
-});
+    const notionResponse = await queryDatabase(res, DATABASE_IDS[req.params.database])
 
-app.get('/tools', async (req, res) => {
-    const notionResponse = await queryDatabase(res, process.env.NOTION_DATABASE_ID_TOOLS)
+    const pages = notionResponse.results.map(page => {
+        let item = {
+            name: page.properties.Name.title[0].plain_text
+        }
 
-    const pages = notionResponse.results.map(page => {return {
-        name: page.properties.Name.title[0].plain_text
-    }})
+        if(page.properties.hasOwnProperty("Experience /10")) {
+            item.exp = page.properties["Experience /10"].number
+        }
+
+        if(page.properties.hasOwnProperty("Field")) {
+            item.field = page.properties.Field.select.name
+        }
+
+        return item
+    })
 
     res.send(pages)
 });
