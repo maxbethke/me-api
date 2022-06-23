189ed1b5434828ed4b551b778309328266384dc5
Maximilian Bethke <maxbethke@web.de>
2022-06-23 10:59:14 +0200

    ME-10: portfolio endpoint

--- src/index.js
+++ src/index.js
@@ -12,22 +12,22 @@ const port = 3001
 const notion = new Client({ auth: process.env.NOTION_KEY });
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-const EXPERIENCE_DATABASES = [
-    {path: '/techstack', databaseId: process.env.NOTION_DATABASE_ID_TECHSTACK},
-    {path: '/tools', databaseId: process.env.NOTION_DATABASE_ID_TOOLS},
-    {path: '/languages', databaseId: process.env.NOTION_DATABASE_ID_LANGUAGES},
-    {path: '/softskills', databaseId: process.env.NOTION_DATABASE_ID_SOFTSKILLS}
+const DATABASES = [
+    {path: '/techstack', databaseId: process.env.NOTION_DATABASE_ID_TECHSTACK, processingFunction: getExperience},
+    {path: '/tools', databaseId: process.env.NOTION_DATABASE_ID_TOOLS, processingFunction: getExperience},
+    {path: '/languages', databaseId: process.env.NOTION_DATABASE_ID_LANGUAGES, processingFunction: getExperience},
+    {path: '/softskills', databaseId: process.env.NOTION_DATABASE_ID_SOFTSKILLS, processingFunction: getExperience},
+    {path: '/projects', databaseId: process.env.NOTION_DATABASE_ID_PROJECTS, processingFunction: getProjects}
 ]
-const PROJECT_DATABASE_ID = process.env.NOTION_DATABASE_ID_PROJECTS
 
 app.get(
-    EXPERIENCE_DATABASES.map(item => item.path),
+    DATABASES.map(item => item.path),
     async (req, res) => {
+        const database = DATABASES.find(item => item.path===req.path)
+
         try {
-            res.send(await getExperience(req.path))
+            res.send(await database.processingFunction(database))
         } catch (e) {
             console.error(e)
             if(e instanceof UpstreamServiceError) {
@@ -40,8 +40,7 @@ app.get(
 );
 
 async function getExperience(database) {
-    const databaseId = EXPERIENCE_DATABASES.find(item => item.path===database).databaseId
-    const notionResponse = await queryDatabase(databaseId)
+    const notionResponse = await queryDatabase(database)
 
     let pages = notionResponse.results.filter(page => page.properties.Name.title.length > 0)
 
@@ -64,12 +63,100 @@ async function getExperience(database) {
     return pages
 }
 
-async function queryDatabase(databaseId, filter = undefined, sorts = undefined) {
+async function getProjects(database) {
+    const notionResponse = await queryDatabase(database)
+
+    let pages = notionResponse.results.filter(page => page.properties.Name.title.length > 0)
+    let mappedPages = []
+
+    for(const page of pages) {
+        let mappedPage = await getProjectProperties(page)
+        mappedPage.contentBlocks = await getProjectContentBlocks(page)
+        mappedPages.push(mappedPage)
+    }
+
+    return mappedPages
+}
+
+async function getProjectProperties(page) {
+    let item = {
+        name: page.properties.Name.title[0].plain_text
+    }
+
+    for (const propName in page.properties) {
+        const prop = page.properties[propName]
+        switch (prop.type) {
+            case 'relation':
+                const relatedPages = []
+
+                for(const relation of prop.relation) {
+                    relatedPages.push(await getNotionPage(relation.id))
+                }
+                item[propName.toLowerCase()] = relatedPages.map(page => ({
+                    name: page.properties.Name.title[0].plain_text,
+                    field: page.properties.Field ? page.properties.Field.select.name : null
+                }))
+                break;
+            case 'select':
+                item[propName.toLowerCase()] = {
+                    text: prop.select.name,
+                    color: prop.select.color
+                }
+                break;
+            case 'date':
+                let date = null
+                if(prop.date) date = prop.date.start
+                item[propName.toLowerCase()] = date
+                break;
+        }
+    }
+
+    return item
+}
+
+async function getProjectContentBlocks(page) {
+    let blocks = await getNotionBlocks(page.id)
+
+    blocks = blocks.results
+    blocks = blocks.map(block => getBlockContent(block))
+
+    return blocks
+}
+
+function getBlockContent(block) {
+    if(block.object !== 'block') throw Error('Tried to get content of non-block')
+    return {
+        type: block.type,
+        content: block[block.type].rich_text
+    }
+}
+
+async function queryDatabase(database, filter = undefined, sorts = undefined) {
     try {
         return await notion.databases.query({
-            database_id: databaseId,
+            database_id: database.databaseId,
             filter: filter,
             sorts: sorts,
+        });
+    } catch (e) {
+        throw new UpstreamServiceError(e)
+    }
+}
+
+async function getNotionPage(pageId) {
+    try {
+        return await notion.pages.retrieve({
+            page_id: pageId
+        });
+    } catch (e) {
+        throw new UpstreamServiceError(e)
+    }
+}
+
+async function getNotionBlocks(blockId) {
+    try {
+        return await notion.blocks.children.list({
+            block_id: blockId
         });
     } catch (e) {
         throw new UpstreamServiceError(e)
